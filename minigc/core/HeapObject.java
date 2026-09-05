1bd882beb8653dfa2774e79d8e99f871a53a1c88
Keshav <keshavvarma899@gmail.com>
2026-09-05 17:58:59 -0500

    Creating framework

---
+++ minigc/core/HeapObject.java
@@ -0,0 +1,20 @@
+public class HeapObject { 
+    
+    public HeapObject(int id,  ){
+
+    }
+
+    private final id = 0;
+    private String name;
+    private boolean marked;
+    private List<Integer> refrences;
+
+
+
+
+
+
+private
+
+
+} 
